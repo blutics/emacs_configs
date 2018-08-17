cb94b6a585fdd418eb8750a9d8e230272dba689b
Daegun <blutics@gmail.com>
2018-08-17 12:47:27 -1000

    changed

--- init.el
+++ init.el
@@ -24,12 +24,13 @@
  '(custom-safe-themes
    (quote
     ("e4cbf084ecc5b7d80046591607f321dd655ec1bbb2dbfbb59c913623bf89aa98" "12394b50f2dfe62d87eba0773765f287d1117e73402b595ccb13f0d6b779ecdd" "a95d66071817d19102f6a5579c23fa90dcddcecb06d9a1f1f94dbc20cf596df7" "871dcace9f5933d13cd2b0ccc806d561cc9615b43fffad9d06ea3327e81757c9" default)))
+ '(eclim-eclipse-dirs (quote ("~/java-photon/eclipse")))
  '(eclim-executable "/home/blutics/.p2/pool/plugins/org.eclim_2.8.0/bin/eclim")
  '(elim-eclipse-dirs (quote ("~/eclipse/java-latest-released/eclipse")))
  '(jdee-server-dir "~/.emacs.d/jdee-server/target")
  '(package-selected-packages
    (quote
-    (memoize dash jdee ac-emacs-eclim company-emacs-eclim highlight highlight-defined highlight-numbers highlight-operators highlight-parentheses highlight-quoted cider yasnippet-snippets undo-tree ibuffer-projectile ibuffer-sidebar multiple-cursors dired-sidebar dired-subtree helm-swoop helm-projectile helm projectile move-text hydra elpy neotree auto-complete counsel ace-window which-key try paredit beacon hungry-delete expand-region flx-ido zerodark-theme use-package moe-theme color-theme-modern challenger-deep-theme base16-theme alect-themes))))
+    (helm-pydoc helm-proc ac-helm hlinum helm-gtags memoize dash jdee ac-emacs-eclim company-emacs-eclim highlight highlight-defined highlight-numbers highlight-operators highlight-parentheses highlight-quoted cider yasnippet-snippets undo-tree ibuffer-projectile ibuffer-sidebar multiple-cursors dired-sidebar dired-subtree helm-swoop helm-projectile helm projectile move-text hydra elpy neotree auto-complete counsel ace-window which-key try paredit beacon hungry-delete expand-region flx-ido zerodark-theme use-package moe-theme color-theme-modern challenger-deep-theme base16-theme alect-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
